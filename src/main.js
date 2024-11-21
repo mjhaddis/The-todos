c4bded374f8b6f5d839f2c0666f2c34ecadaa923
Michael <mjhaddis@gmail.com>
2024-11-22 00:52:48 +0100

    added checkbox and delete

--- src/main.js
+++ src/main.js
@@ -20,22 +20,43 @@ function addTodo() {
         if (todoText) {
 
             const newTodo = new Todo(Date.now(), todoText);
-
             notDoneArrayList.push(newTodo);
 
             const notDoneListItem = document.createElement("li");
-            notDoneListItem.textContent = newTodo.text;
             notDoneListItem.id = newTodo.id;
+
+            const doneCheckbox = document.createElement("input");
+            doneCheckbox.setAttribute("type", "checkbox");
+
+            const todoTextNode = document.createTextNode(newTodo.text);
+
+            const deleteTodo = document.createElement("span");
+            deleteTodo.innerHTML = "x";
+            
+            deleteTodo.addEventListener("click", () => {
+                //Ta bort från DOM
+                notDoneListItem.remove();
+            })
+            
+            notDoneListItem.appendChild(doneCheckbox);
+            notDoneListItem.appendChild(todoTextNode);
+            notDoneListItem.appendChild(deleteTodo)
+
             notDoneList.appendChild(notDoneListItem);
 
             textInput.value = "";
 
         } else {
-            alert("Please enter some text!");
+            alert("Please enter something to do!");
         }
         
     });
     
+}
+
+function doneTodo() {
+
+
 }
 
 addTodo();
